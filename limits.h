b8b033545155785d2a68f33c604ea84724478d3b
Tom Niesse <tomniesse@gmail.com>
2025-03-28 21:41:11 +0100

    initial commit

--- limits.h
+++ limits.h
@@ -1,8 +1,7 @@
 #ifndef LIMITS_H
 #define LIMITS_H
 
-#define MAX_CONNECTION_TIMEOUT 35000 // milliseconds
+#define MAX_CONNECTION_TIMEOUT 5000 // milliseconds
 #define MAX_ROUNDTRIP_TIMEOUT 1 // milliseconds
-#define THREAD_SLEEP_TIME 250 // microseconds
 
 #endif // LIMITS_H
